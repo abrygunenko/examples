c65ba25ff6c39996a517235b4f631bf24d7674a2
Anton Brygunenko <abryg@softserveinc.com>
2017-03-01 02:13:41 +0100

    Added page factory example and test

--- src/main/java/com/softserve/crashcourse/session12/example1/webpages/SeleniumHQAboutWebPage.java
+++ src/main/java/com/softserve/crashcourse/session12/example1/webpages/SeleniumHQAboutWebPage.java
@@ -1,14 +1,27 @@
 package com.softserve.crashcourse.session12.example1.webpages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SeleniumHQAboutWebPage extends AbstractSeleniumHQWebPage {
+
+    @FindBy(css = "h3 > a[href='/docs'")
+    private WebElement documentationLink;
 
     public SeleniumHQAboutWebPage() {
         super("About Selenium", "about/");
+        PageFactory.initElements(getWebDriver(), this);
     }
 
     @Override
     public SeleniumHQAboutWebPage initializePage() {
         super.initializePage();
         return this;
+    }
+
+    public SeleniumHQDocumentsWebPage useDocumentationLink() {
+        documentationLink.click();
+        return new SeleniumHQDocumentsWebPage().initializePage();
     }
 }
---
+++ src/test/java/com/softserve/crashcourse/session14/example3/PageFactoryTest.java
@@ -0,0 +1,15 @@
+package com.softserve.crashcourse.session14.example3;
+
+import com.softserve.crashcourse.session12.example1.BaseWebTest;
+import com.softserve.crashcourse.session12.example1.webpages.*;
+import org.testng.annotations.Test;
+
+public class PageFactoryTest extends BaseWebTest {
+
+    @Test
+    public void pageFactoryTest() {
+        SeleniumHQAboutWebPage seleniumHQAboutWebPage = new SeleniumHQAboutWebPage();
+        seleniumHQAboutWebPage.openPage();
+        seleniumHQAboutWebPage.useDocumentationLink();
+    }
+}
